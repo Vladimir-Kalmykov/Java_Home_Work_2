14bd97c96d707891fd01ed7bcc0d712e3344f50b
Vladimir <bob.kalmykov@mail.ru>
2023-06-18 23:39:41 +0300

    Initial commit

---
+++ src/main/java/Home_Worke_2.java
@@ -0,0 +1,28 @@
+// Задание 1: Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+// используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+public class Home_Worke_2 {
+    public static void main(String[] args) {
+            String s = "select * from students where ";
+            StringBuilder sb = new StringBuilder(s);
+            String jsonFilter = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+            jsonFilter = jsonFilter
+                    .replace("{", "")
+                    .replace("}", "")
+                    .replace("\"", "");
+            String[] jsonFilterArray = jsonFilter.split(", ");
+
+            for (String columnValue : jsonFilterArray) {
+                String[] columnValueArray = columnValue.split(":");
+
+                if (columnValueArray[1].equals("null")) {
+                    continue;
+                }
+                sb.append(columnValueArray[0]).append(" = '").append(columnValueArray[1]).append("' and ");
+            }
+            sb.delete(sb.lastIndexOf("'") + 1, sb.length()).append(";");
+            System.out.println("SQL запрос: " + sb);
+        }
+}
